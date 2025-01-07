3861a27495a32bfa8eeb1aae13c5b3d868a94955
1Jerome23 <jerome_victoria@dlsu.edu.ph>
2025-01-07 22:42:06 +0800

    Firebase + Login/Signup
    Just need to edit design, working in firebase users.
    Passwords are also hashed.

---
+++ fitride/lib/widget_tree.dart
@@ -0,0 +1,27 @@
+import 'package:fitride/auth.dart';
+import 'package:fitride/pages/home_page.dart';
+import 'package:fitride/pages/login_register.dart';
+import 'package:flutter/material.dart';
+
+class WidgetTree extends StatefulWidget {
+  const WidgetTree({Key? key}) : super(key: key);
+
+  @override
+  State<WidgetTree> createState() => _WidgetTreeState();
+}
+
+class _WidgetTreeState extends State<WidgetTree> {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: Auth().authStateChanges,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return HomePage();
+        } else {
+          return const LoginPage();
+        }
+      },
+    );
+  }
+}
