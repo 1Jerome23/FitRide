b20487f9c92aee5fd013d2eeaf560cb1c74cf819
1Jerome23 <jerome_victoria@dlsu.edu.ph>
2025-01-10 00:11:20 +0800

    signup login page edited

--- fitride/lib/pages/login_register.dart
+++ fitride/lib/pages/login_register.dart
@@ -3,7 +3,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:fitride/auth.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
@@ -50,15 +49,16 @@ class _LoginPageState extends State<LoginPage> {
       });
     }
   }
+
   Widget _submitButton() {
-  return ElevatedButton(
-    onPressed: isLogin ? signInWithEmailAndPassword : createUserWithEmailAndPassword,
-    child: Text(isLogin ? 'Login' : 'Register'),
-    style: ElevatedButton.styleFrom(
-      padding: EdgeInsets.symmetric(vertical: 12, horizontal: 30),
-    ),
-  );
-}
+    return ElevatedButton(
+      onPressed: isLogin ? signInWithEmailAndPassword : createUserWithEmailAndPassword,
+      child: Text(isLogin ? 'Login' : 'Register'),
+      style: ElevatedButton.styleFrom(
+        padding: EdgeInsets.symmetric(vertical: 12, horizontal: 30),
+      ),
+    );
+  }
 
 Widget _loginOrRegisterButton() {
   return TextButton(
@@ -67,19 +67,36 @@ Widget _loginOrRegisterButton() {
         isLogin = !isLogin;
       });
     },
-    child: Text(isLogin ? 'Register instead' : 'Login instead'),
+    child: RichText(
+      text: TextSpan(
+        text: isLogin ? "Don't have an account? " : "Already have an account? ",
+        style: TextStyle(color: Colors.white),
+        children: [
+          TextSpan(
+            text: isLogin ? 'Register instead' : 'Login instead',
+            style: TextStyle(color: Colors.yellow[700], fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    ),
   );
 }
-  Widget _entryField(String title, TextEditingController controller, {bool isPassword = false}) {
-    return TextField(
-      controller: controller,
-      obscureText: isPassword,
-      decoration: InputDecoration(
-        labelText: title,
-        border: OutlineInputBorder(),
+
+Widget _entryField(String title, TextEditingController controller, {bool isPassword = false}) {
+  return TextField(
+    controller: controller,
+    obscureText: isPassword,
+    decoration: InputDecoration(
+      labelText: title,
+      floatingLabelBehavior: FloatingLabelBehavior.never, 
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(15), 
       ),
-    );
-  }
+      filled: true, 
+      fillColor: Colors.white,
+    ),
+  );
+}
 
   Widget _errorMessage() {
     return errorMessage == null || errorMessage!.isEmpty
@@ -87,100 +104,112 @@ Widget _loginOrRegisterButton() {
         : Text('Hmm? $errorMessage', style: TextStyle(color: Colors.red));
   }
 
- Widget _socialButton(String text, Color color, IconData icon, VoidCallback onPressed) {
-  return ElevatedButton.icon(
-    onPressed: onPressed,
-    icon: Icon(icon, color: Colors.white),  
-    label: Text(
-      text,
-      style: TextStyle(color: Colors.white),  
-    ),
-    style: ElevatedButton.styleFrom(
-      backgroundColor: color,
-      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-    ),
-  );
-}
+  Widget _socialButton(String text, Color color, IconData icon, VoidCallback onPressed) {
+    return ElevatedButton.icon(
+      onPressed: onPressed,
+      icon: Icon(icon, color: Colors.white),
+      label: Text(
+        text,
+        style: TextStyle(color: Colors.white),
+      ),
+      style: ElevatedButton.styleFrom(
+        backgroundColor: color,
+        padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      ),
+    );
+  }
 
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    body: SingleChildScrollView( 
-      child: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Colors.orange, Colors.white],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [const Color.fromARGB(255, 56, 56, 56), Color(0xFF383838)],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.only(top: 30.0),
+                child: Text(
+                  'Welcome to',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold,
+                  color: Colors.white),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 5.0),
+                child: Image.asset(
+                  'assets/fitride_logo.png',
+                  width: 350.0,
+                  height: 150.0,
+                ),
+              ),
+              SizedBox(height: 15),
+              Container(
+                width: 350,
+                decoration: BoxDecoration(
+                  color: Colors.white, // Set background color to white
+                  borderRadius: BorderRadius.circular(30),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: Offset(0, 3), // Shadow direction
+                    ),
+                  ],
+                ),
+                child: _entryField('Email', _controllerEmail),
+              ),
+              SizedBox(height: 15),
+              Container(
+                width: 350,
+                decoration: BoxDecoration(
+                  color: Colors.white, // Set background color to white
+                  borderRadius: BorderRadius.circular(30),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: Offset(0, 3), // Shadow direction
+                    ),
+                  ],
+                ),
+                child: _entryField('Password', _controllerPassword, isPassword: true),
+              ),
+              Align(
+                alignment: Alignment.centerRight,
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text('Forgot Password?', style: TextStyle(color: Colors.blue)),
+                ),
+              ),
+              _errorMessage(),
+              SizedBox(height: 10),
+              Container(
+                width: 300,
+                child: _submitButton(),
+              ),
+              SizedBox(height: 5),
+              Container(
+                width: 300,
+                child: _loginOrRegisterButton(),
+              ),
+              SizedBox(width: 350, child: _socialButton('Connect with Facebook', Colors.blueAccent, Icons.facebook, () {})),
+              SizedBox(height: 10),
+              SizedBox(width: 350, child: _socialButton('Connect with Google', Colors.red, FontAwesomeIcons.google, () {})),
+            ],
           ),
         ),
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              margin: const EdgeInsets.only(top: 30.0),
-              child: Text(
-                'Welcome to',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-            ),
-            Container(
-              margin: const EdgeInsets.only(top: 5.0),
-              child: Text(
-                'FitRide',
-                style: TextStyle(fontSize: 45, fontWeight: FontWeight.bold),
-              ),
-            ),
-            SizedBox(height: 15),
-            Container(
-              width: 350,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30),
-              ),
-              child: _entryField('Email', _controllerEmail),
-            ),
-            SizedBox(height: 15),
-            Container(
-              width: 350,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30),
-              ),
-              child: _entryField('Password', _controllerPassword, isPassword: true),
-            ),
-            Align(
-              alignment: Alignment.centerRight,
-              child: TextButton(
-                onPressed: () {},
-                child: Text('Forgot Password?', style: TextStyle(color: Colors.blue)),
-              ),
-            ),
-            _errorMessage(),
-            SizedBox(height: 10),
-            Container(
-              width: 300,
-              child: _submitButton(),
-            ),
-            SizedBox(height: 5),
-            Container(
-              width: 300,
-              child: _loginOrRegisterButton(),
-            ),
-            SizedBox(width: 350, child: _socialButton('Connect with Facebook', Colors.blueAccent, Icons.facebook, () {})),
-            SizedBox(height: 10),
-            SizedBox(width: 350, child: _socialButton('Connect with Google', Colors.red, FontAwesomeIcons.google, () {})),
-            Image.asset(
-              'assets/bicycle_home.png',
-              height: 100,
-            ),
-          ],
-        ),
       ),
-    ),
-  );
+    );
+  }
 }
-
-}
-
-
-//login register UI
