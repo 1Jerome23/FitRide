9eff4889540e12222ce8dac50b03346cb1b101cd
1Jerome23 <jerome_victoria@dlsu.edu.ph>
2025-01-12 22:53:31 +0800

    changed default theme + added homepage stuff
    Need to improve design and add features

--- fitride/lib/main.dart
+++ fitride/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:fitride/widget_tree.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -18,9 +17,32 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.orange,
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: Color(0xFFF89C23),
+          secondary: Color(0xFFF89C23),
+        ),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        textTheme: Theme.of(context).textTheme.apply(
+              bodyColor: Colors.black,
+              displayColor: Colors.black,
+            ),
+        appBarTheme: AppBarTheme(
+          backgroundColor: Color(0xFFF89C23),
+          elevation: 5,
+          titleTextStyle: TextStyle(color: Colors.white, fontSize: 20),
+        ),
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          selectedItemColor: Color(0xFFF89C23), 
+          unselectedItemColor: Colors.grey,
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Color(0xFFF89C23)), 
+            foregroundColor: MaterialStateProperty.all(Colors.white), 
+          ),
+        ),
       ),
       home: const WidgetTree(),
     );
   }
 }
-
--- fitride/lib/pages/home_page.dart
+++ fitride/lib/pages/home_page.dart
@@ -3,6 +3,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:geolocator/geolocator.dart';
+import 'dart:async';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,31 +16,45 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'FitRide',
       theme: ThemeData(
-        primaryColor: Color(0xFFF89C23), 
+        primaryColor: Color(0xFFF89C23),
         colorScheme: ColorScheme.fromSwatch().copyWith(
-          primary: Color(0xFFF89C23), 
-          secondary: Color(0xFFF89C23), 
+          primary: Color(0xFFF89C23),
+          secondary: Color(0xFFF89C23),
         ),
         visualDensity: VisualDensity.adaptivePlatformDensity,
-        textTheme: TextTheme(
-          bodyLarge: TextStyle(color: Colors.black),
-          titleLarge: TextStyle(color: Colors.white),
+        textTheme: GoogleFonts.openSansTextTheme(
+          Theme.of(context).textTheme.apply(
+                bodyColor: Colors.black,
+                displayColor: Colors.black,
+              ),
         ),
         appBarTheme: AppBarTheme(
-          backgroundColor: Color(0xFFF89C23), 
+          backgroundColor: Color(0xFFF89C23),
+          elevation: 5,
+          titleTextStyle: GoogleFonts.poppins(
+            textStyle: TextStyle(color: Colors.white, fontSize: 20),
+          ),
         ),
         bottomNavigationBarTheme: BottomNavigationBarThemeData(
-          selectedItemColor: Color(0xFFF89C23),  
-          unselectedItemColor: Colors.grey,      
+          selectedItemColor: Color(0xFFF89C23),
+          unselectedItemColor: Colors.grey,
         ),
         buttonTheme: ButtonThemeData(
-          buttonColor: Color(0xFFF89C23),      
+          buttonColor: Color(0xFFF89C23),
+          textTheme: ButtonTextTheme.primary, 
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Color(0xFFF89C23), 
+            foregroundColor: Colors.white,
+          ),
         ),
       ),
       home: HomePage(),
     );
   }
 }
+
 
 class HomePage extends StatefulWidget {
   HomePage({Key? key}) : super(key: key);
@@ -51,33 +67,42 @@ class _HomePageState extends State<HomePage> {
   late SharedPreferences _prefs;
   String _name = '';
   String _weatherMessage = '';
-  String _weatherImage = '';  
+  String _weatherImage = '';
   bool isLoading = true;
   bool locationError = false;
   bool weatherError = false;
-  int _selectedIndex = 0; 
+  int _selectedIndex = 0;
+  List<String> cyclingRecommendations = [
+    "Make sure to stay hydrated during your ride!",
+    "Always wear a helmet for safety.",
+    "Start with a warm-up before any cycling session.",
+    "Track your progress to stay motivated!",
+    "Plan your cycling route ahead for a smooth ride.",
+    "Don’t forget to take breaks during long rides.",
+    "Check your tire pressure before heading out."
+  ];
+  int _currentRecommendationIndex = 0;
 
   @override
   void initState() {
     super.initState();
     _loadPreferences();
     _fetchWeather();
+    _loadRecommendationIndex();
+    _startRecommendationTimer();
   }
 
-  // Load user preferences
   Future<void> _loadPreferences() async {
     _prefs = await SharedPreferences.getInstance();
     setState(() {
       _name = _prefs.getString('userName') ?? '';
     });
 
-    // If name is not set, ask for it
     if (_name.isEmpty) {
       _askForName();
     }
   }
 
-  // Ask the user for their name
   void _askForName() {
     showDialog(
       context: context,
@@ -103,7 +128,6 @@ class _HomePageState extends State<HomePage> {
     );
   }
 
-  // Save name to SharedPreferences
   Future<void> _saveName(String name) async {
     _prefs.setString('userName', name);
     setState(() {
@@ -111,12 +135,11 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
-  // Fetch weather based on the user's current location
   Future<void> _fetchWeather() async {
     Position? position = await _getCurrentLocation();
     if (position != null) {
       final response = await http.get(Uri.parse(
-        'https://api.open-meteo.com/v1/forecast?latitude=${position.latitude}&longitude=${position.longitude}&current_weather=true'
+        'https://api.open-meteo.com/v1/forecast?latitude=${position.latitude}&longitude=${position.longitude}&current_weather=true',
       ));
       if (response.statusCode == 200) {
         final data = json.decode(response.body);
@@ -126,16 +149,16 @@ class _HomePageState extends State<HomePage> {
           setState(() {
             if (weather == 1) {
               _weatherMessage = "It's sunny today! It's a good day to take a ride.";
-              _weatherImage = 'assets/sunny.png'; 
+              _weatherImage = 'assets/sunny.png';
             } else if (weather == 2) {
               _weatherMessage = "It's rainy today. Stay safe if you're heading out!";
-              _weatherImage = 'assets/images/rainy.png';  
+              _weatherImage = 'assets/images/rainy.png';
             } else if (weather == 3) {
               _weatherMessage = "It's cloudy today.";
-              _weatherImage = 'assets/cloudy.png';  
+              _weatherImage = 'assets/cloudy.png';
             } else {
               _weatherMessage = "Stormy weather ahead. Stay safe!";
-              _weatherImage = 'assets/images/stormy.png';  
+              _weatherImage = 'assets/images/stormy.png';
             }
             isLoading = false;
           });
@@ -161,7 +184,6 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
-  // Get the current location using Geolocator
   Future<Position?> _getCurrentLocation() async {
     bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
@@ -185,15 +207,28 @@ class _HomePageState extends State<HomePage> {
     return await Geolocator.getCurrentPosition();
   }
 
-  void _retryFetchWeather() {
+  Future<void> _loadRecommendationIndex() async {
+    _prefs = await SharedPreferences.getInstance();
     setState(() {
-      isLoading = true;
-      locationError = false;
-      weatherError = false;
-      _weatherMessage = '';
-      _weatherImage = ''; 
+      _currentRecommendationIndex = _prefs.getInt('recommendationIndex') ?? 0;
     });
-    _fetchWeather();
+  }
+
+  Future<void> _saveRecommendationIndex(int index) async {
+    _prefs.setInt('recommendationIndex', index);
+  }
+
+  void _startRecommendationTimer() {
+    Timer.periodic(Duration(seconds: 10), (timer) {
+      _showNextRecommendation();
+    });
+  }
+
+  void _showNextRecommendation() {
+    setState(() {
+      _currentRecommendationIndex = (_currentRecommendationIndex + 1) % cyclingRecommendations.length;
+    });
+    _saveRecommendationIndex(_currentRecommendationIndex);
   }
 
   Widget _greeting() {
@@ -202,26 +237,30 @@ class _HomePageState extends State<HomePage> {
       children: [
         Text(
           'Hello, $_name!',
-          style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.black),
+          style: GoogleFonts.poppins(
+            textStyle: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.black),
+          ),
         ),
-        const Text(
+        Text(
           'Welcome to FitRide',
-          style: TextStyle(fontSize: 16, color: Colors.black),
+          style: GoogleFonts.poppins(
+            textStyle: TextStyle(fontSize: 16, color: Colors.black),
+          ),
         ),
         SizedBox(height: 20),
         isLoading
-            ? const CircularProgressIndicator()
+            ? CircularProgressIndicator()
             : locationError
                 ? Column(
                     children: [
                       Text(
                         _weatherMessage,
-                        style: const TextStyle(fontSize: 16, color: Colors.black),
+                        style: TextStyle(fontSize: 16, color: Colors.black),
                       ),
                       SizedBox(height: 10),
                       ElevatedButton(
-                        onPressed: _retryFetchWeather,
-                        child: const Text("Enable Location & Retry"),
+                        onPressed: _fetchWeather,
+                        child: Text("Enable Location & Retry"),
                       ),
                     ],
                   )
@@ -230,25 +269,42 @@ class _HomePageState extends State<HomePage> {
                         children: [
                           Text(
                             _weatherMessage,
-                            style: const TextStyle(fontSize: 16, color: Colors.black),
+                            style: TextStyle(fontSize: 16, color: Colors.black),
                           ),
                           SizedBox(height: 10),
                           ElevatedButton(
-                            onPressed: _retryFetchWeather,
-                            child: const Text("Retry"),
+                            onPressed: _fetchWeather,
+                            child: Text("Retry"),
                           ),
                         ],
                       )
-                    : Column(
-                        children: [
-                          Text(
-                            _weatherMessage,
-                            style: const TextStyle(fontSize: 16, color: Colors.black),
-                          ),
-                          SizedBox(height: 20),
-                          Image.asset(_weatherImage, height: 200),  // Display weather image
-                        ],
+                    : Center(
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              _weatherMessage,
+                              style: TextStyle(fontSize: 16, color: Colors.black),
+                              textAlign: TextAlign.center,
+                            ),
+                            SizedBox(height: 20),
+                            Image.asset(_weatherImage, height: 200),
+                          ],
+                        ),
                       ),
+        SizedBox(height: 30),
+        Text(
+          'Cycling Recommendation:',
+          style: GoogleFonts.poppins(
+            textStyle: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black),
+          ),
+        ),
+        SizedBox(height: 10),
+        Text(
+          cyclingRecommendations[_currentRecommendationIndex],
+          style: TextStyle(fontSize: 16, color: Colors.black),
+        ),
+        SizedBox(height: 20),
       ],
     );
   }
@@ -263,14 +319,23 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('FitRide'),
-        backgroundColor: Theme.of(context).primaryColor,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text('FitRide'),
+            Image.asset(
+              'assets/logobike.png', 
+              height: 40, 
+            ),
+          ],
+        ),
+              backgroundColor: Theme.of(context).primaryColor,
       ),
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex, 
-        selectedItemColor: Theme.of(context).colorScheme.primary, 
+        currentIndex: _selectedIndex,
+        selectedItemColor: Theme.of(context).colorScheme.primary,
         unselectedItemColor: Colors.grey,
-        onTap: _onItemTapped, 
+        onTap: _onItemTapped,
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
