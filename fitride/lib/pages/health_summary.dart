c4fa3b3049b600d690973de900a198ade14c56aa
1Jerome23 <jerome_victoria@dlsu.edu.ph>
2025-02-25 15:11:59 +0800

    base health summary

--- fitride/lib/pages/health_summary.dart
+++ fitride/lib/pages/health_summary.dart
@@ -1,11 +1,7 @@
-import 'package:fitride/pages/goal_tracking.dart';
-import 'package:fitride/pages/home_page.dart';
-import 'package:fitride/pages/profile.dart';
-import 'package:fitride/pages/recommendation.dart';
-import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HealthSummary extends StatefulWidget {
   @override
@@ -13,234 +9,138 @@ class HealthSummary extends StatefulWidget {
 }
 
 class _HealthSummaryState extends State<HealthSummary> {
+Future<Map<String, dynamic>> _fetchData(String userId) async {
+  try {
+    final userDataSnapshot = await FirebaseFirestore.instance.collection('userData').doc(userId).get();
+    final activitiesSnapshot = await FirebaseFirestore.instance.collection('activities').where('user_id', isEqualTo: userId).get();
+    
+    final athleteSnapshot = await FirebaseFirestore.instance.collection('athletes').doc(userId).get();
+    final athleteData = athleteSnapshot.data() ?? {};
 
-  Future<Map<String, dynamic>> _fetchData(String userId) async {
-    try {
-      final userQuestionnaireDoc = FirebaseFirestore.instance
-          .collection('User Questionnaires')
-          .doc(userId); 
-      final activitiesDoc = FirebaseFirestore.instance
-          .collection('activities')
-          .doc(userId); 
-      final afterExerciseFormDoc = FirebaseFirestore.instance
-          .collection('After Exercise Form')
-          .doc(userId); 
+    final userData = userDataSnapshot.data() ?? {};
+    final activities = activitiesSnapshot.docs.map((doc) => doc.data()).toList();
+    
+    double weight = double.tryParse(userData['weight']?.toString() ?? '0') ?? 0;
+    double height = double.tryParse(userData['height']?.toString() ?? '0') ?? 0;
+    double heightInMeters = height / 100;
+    double bmi = (heightInMeters > 0) ? weight / (heightInMeters * heightInMeters) : 0;
+    double metabolicRate = double.tryParse(userData['metabolic_rate']?.toString() ?? '0') ?? 0;
+    double bodyFatPercentage = double.tryParse(userData['body_fat_percentage']?.toString() ?? '0') ?? 0;
 
-      final userQuestionnaireSnapshot = await userQuestionnaireDoc.get();
-      final activitiesSnapshot = await activitiesDoc.get();
-      final afterExerciseFormSnapshot = await afterExerciseFormDoc.get();
-
-      print("User Questionnaire Data: ${userQuestionnaireSnapshot.data()}");
-      print("Activities Data: ${activitiesSnapshot.data()}");
-      print("After Exercise Form Data: ${afterExerciseFormSnapshot.data()}");
-
-      return {
-        'User Questionnaires': userQuestionnaireSnapshot.data(),
-        'activities': activitiesSnapshot.data(),
-        'afterExerciseForm': afterExerciseFormSnapshot.data(),
-      };
-    } catch (e) {
-      throw Exception("Error fetching data: $e");
+    double totalHeartRate = 0;
+    double maxHeartRate = 0;
+    double totalDistance = 0;
+    double totalCalories = 0;
+    int heartRateCount = 0;
+    
+    for (var activity in activities) {
+      double heartRate = double.tryParse(activity['average_heartrate']?.toString() ?? '0') ?? 0;
+      double distance = double.tryParse(activity['distance']?.toString() ?? '0') ?? 0;
+      double calories = double.tryParse(activity['calories_burned']?.toString() ?? '0') ?? 0;
+      
+      if (heartRate > 0) {
+        totalHeartRate += heartRate;
+        heartRateCount++;
+      }
+      if (heartRate > maxHeartRate) {
+        maxHeartRate = heartRate;
+      }
+      totalDistance += distance;
+      totalCalories += calories;
     }
+    
+    double averageHeartRate = (heartRateCount > 0) ? totalHeartRate / heartRateCount : 0;
+    
+    return {
+      'athlete_name': athleteData['athlete_name'] ?? 'N/A',  // ✅ Fetch correctly
+      'age': userData['age']?.toString() ?? 'N/A',
+      'height': height.toString(),
+      'weight': weight.toString(),
+      'bmi': bmi.toStringAsFixed(2),
+      'metabolic_rate': metabolicRate.toStringAsFixed(2),
+      'body_fat_percentage': bodyFatPercentage.toStringAsFixed(2),
+      'average_heart_rate': averageHeartRate.toStringAsFixed(2),
+      'max_heart_rate': maxHeartRate.toStringAsFixed(2),
+      'total_distance': totalDistance.toStringAsFixed(2),
+      'total_calories': totalCalories.toStringAsFixed(2),
+    };
+  } catch (e) {
+    throw Exception("Error fetching data: $e");
   }
+}
 
- @override
+
+  @override
   Widget build(BuildContext context) {
     final user = FirebaseAuth.instance.currentUser;
-    final userId = user?.uid ?? ''; 
+    final userId = user?.uid ?? '';
 
     return Scaffold(
       appBar: AppBar(
-        automaticallyImplyLeading: false,
-        backgroundColor: Theme.of(context).primaryColor,
-        title: Text(
-          "FitRide",
-          style: GoogleFonts.roboto(
-            color: Colors.orange,
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        actions: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Image.asset(
-              'assets/logobike.png',
-              height: 40,
-            ),
-          ),
-        ],
+        title: Text("Health Summary", style: GoogleFonts.roboto(fontSize: 24, fontWeight: FontWeight.bold, color: Colors.black)),
+        backgroundColor: Colors.white,
+        elevation: 1,
+        iconTheme: IconThemeData(color: Colors.black),
       ),
       body: FutureBuilder<Map<String, dynamic>>(
-        future: _fetchData(userId), 
+        future: _fetchData(userId),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return Center(child: CircularProgressIndicator());
           }
-
           if (snapshot.hasError) {
             return Center(child: Text('Error: ${snapshot.error}'));
           }
-
           if (!snapshot.hasData) {
             return Center(child: Text('No data available.'));
           }
 
           final data = snapshot.data!;
 
-          double weight = double.tryParse(data['User Questionnaires']?['weight']?.toString() ?? '0') ?? 0;
-          double height = double.tryParse(data['User Questionnaires']?['height']?.toString() ?? '0') ?? 0;
-
-          double heightInMeters = height / 100;
-
-          double bmi = (heightInMeters > 0) ? weight / (heightInMeters * heightInMeters) : 0;
-
-
-          var activities = data['activities']?['activities'] ?? [];
-          double totalHeartRate = 0;
-          int count = 0;
-
-          for (var activity in activities) {
-            double heartRate = double.tryParse(activity['average_heartrate']?.toString() ?? '0') ?? 0;
-            if (heartRate > 0) {
-              totalHeartRate += heartRate;
-              count++;
-            }
-          }
-          double averageHeartRate = (count > 0) ? totalHeartRate / count : 0;  
-
           return Padding(
             padding: const EdgeInsets.all(16.0),
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  _buildSectionTitle("Personal"),
-                  _buildInfoRow("Name", data['User Questionnaires']?['name']?.toString() ?? 'N/A'),
-                  _buildInfoRow("Weight", data['User Questionnaires']?['weight']?.toString() ?? 'N/A'),
-                  _buildInfoRow("Height", data['User Questionnaires']?['height']?.toString() ?? 'N/A'),
-                  _buildInfoRow("BMI", bmi > 0 ? bmi.toStringAsFixed(2) : 'Invalid data'),
-                  _buildInfoRow("Age", data['User Questionnaires']?['age']?.toString() ?? 'N/A'),
-                  _buildInfoRow("Body Water", data['User Questionnaires']?['bodyWater']?.toString() ?? 'N/A'),
-
-                  _buildSectionTitle("Heart Health"),
-                  _buildInfoRow("Consistent Heart Rate", averageHeartRate > 0 ? averageHeartRate.toStringAsFixed(2) : '0'),
-                  _buildInfoRow("Have you experienced heart issues?", data['User Questionnaires']?['experiencedHeartRateIssues'] ?? 'N/A'),
-                ],
-              ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                _buildCard("Athlete Name", data['athlete_name']),
+                _buildCard("Age", data['age']),
+                _buildCard("Height (cm)", data['height']),
+                _buildCard("Weight (kg)", data['weight']),
+                _buildCard("BMI", data['bmi']),
+                _buildCard("Metabolic Rate", data['metabolic_rate']),
+                _buildCard("Body Fat Percentage", data['body_fat_percentage']),
+                _buildCard("Average Heart Rate", data['average_heart_rate']),
+                _buildCard("Max Heart Rate", data['max_heart_rate']),
+                _buildCard("Total Distance Cycled (km)", data['total_distance']),
+                _buildCard("Total Calories Burned", data['total_calories']),
+              ],
             ),
           );
         },
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 3,  
-        selectedItemColor: Theme.of(context).colorScheme.primary,
-        unselectedItemColor: Colors.grey,
-        onTap: (index) {
-          switch (index) {
-            case 0:
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(builder: (context) => HomePage()),
-              );
-              break;
-            case 1:
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(builder: (context) => RecommendationPage()),
-              );
-              break;
-            case 2:
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(builder: (context) => GoalTrackingPage()),
-              );
-              break;
-            case 3:
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(builder: (context) => ProfilePage()),
-              );
-              break;
-          }
-        },
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.insights),
-            label: 'Insights',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.data_usage),
-            label: 'Goal/Progress',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-          ),
-        ],
-      ),
     );
   }
 
-Widget _buildSectionTitle(String title) {
-  return Padding(
-    padding: const EdgeInsets.symmetric(vertical: 12.0),
-    child: Row(
-      children: [
-        Icon(
-          Icons.info_outline,
-          color: Theme.of(context).primaryColor,
-          size: 24,
-        ),
-        SizedBox(width: 8),
-        Text(
-          title,
-          style: GoogleFonts.roboto(
-            fontSize: 22,
-            fontWeight: FontWeight.bold,
-            color: Theme.of(context).primaryColor,
-          ),
-        ),
-      ],
-    ),
-  );
-}
-
-Widget _buildInfoRow(String label, String value) {
-  return Padding(
-    padding: const EdgeInsets.symmetric(vertical: 15.0),
-    child: Row(
-      children: [
-        Container(
-          width: 180,
-          child: Text(
-            "$label: ",
-            style: GoogleFonts.roboto(
-              fontSize: 18,
-              fontWeight: FontWeight.w600,
-              color: Colors.black87,
+  Widget _buildCard(String label, String value) {
+    return Card(
+      elevation: 2,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "$label:",
+              style: GoogleFonts.roboto(fontSize: 18, fontWeight: FontWeight.w600, color: Colors.black),
             ),
-          ),
-        ),
-        Expanded(
-          child: Text(
-            value,
-            style: GoogleFonts.roboto(
-              fontSize: 18,
-              color: Colors.black54,
-              fontWeight: FontWeight.w500,
+            Text(
+              value,
+              style: GoogleFonts.roboto(fontSize: 18, fontWeight: FontWeight.w400, color: Colors.black87),
             ),
-            overflow: TextOverflow.ellipsis, 
-            softWrap: true,
-          ),
+          ],
         ),
-      ],
-    ),
-  );
-}
-
+      ),
+    );
+  }
 }
